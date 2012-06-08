c689604206937b32e3e7ea928d78a7156cd4be78
morganrallen <morganrallen@gmail.com>
2012-06-07 23:50:18 -0500

    switch tests to vows

--- test/tests.js
+++ test/tests.js
@@ -1,67 +1,94 @@
-var Backbone = require("backbone");
+var Backbone = require("backbone"),
+	assert = require("assert"),
+	vows = require("vows");
 require("../")(Backbone);
 
-test("Bootstapping completed", function() {
-	ok(typeof(Backbone.Collections) !== undefined, "Backbone.Collections exists");
-	ok(typeof(Backbone.Models) !== undefined, "Backbone.Models exists");
-	ok(typeof(Backbone.Views) !== undefined, "Backbone.Views exists");
-	ok(typeof(Backbone.Routers) !== undefined, "Backbone.Routers exists");
-});
+vows.describe("Bootstapping completed").addBatch({
+	"Object Exists": {
+		topic: Backbone,
 
+		"Backbone.Collections exists": function(b) { assert.notEqual(undefined, Backbone.Collections); },
+		"Backbone.Models exists": function(b) { assert.notEqual(undefined, Backbone.Models); },
+		"Backbone.Views exists": function(b) { assert.notEqual(undefined, Backbone.Views); },
+		"Backbone.Routers exists": function(b) { assert.notEqual(undefined, Backbone.Routers); }
+	}
+}).run();
 
-test("Creating Named Classes", function() {
-	Backbone.Collection.define("NamedCollection", {
-		initialize: function(models, options) {
+vows.describe("Creating Named Classes").addBatch({
+	".Collections.NamedCollection exists": function() {
+		Backbone.Collection.define("NamedCollection", {
+			initialize: function(models, options) {
 
-		}
-	}, {
-		classProperty: true
-	});
-	ok(typeof(Backbone.Collections.NamedCollection) !== undefined, ".Collections.NamedCollection exists");
+			}
+		}, {
+			classProperty: true
+		});
+		
+		assert.notEqual(Backbone.Collections.NamedCollection, undefined);
+	},
 
-	Backbone.Model.define("NamedModel", {
-		initialize: function(models, options) {
+	".Models.NamedModel exists": function() {
+		Backbone.Model.define("NamedModel", {
+			initialize: function(models, options) {
 
-		}
-	}, {
-		classProperty: true
-	});
-	ok(typeof(Backbone.Models.NamedModel) !== undefined, ".Models.NamedModel exists");
+			}
+		}, {
+			classProperty: true
+		});
+		
+		assert.notEqual(Backbone.Models.NamedModel, undefined)
+	},
 
-	Backbone.View.define("NamedView", {
-		initialize: function(models, options) {
+	".Views.NamedView exists": function() {
+		Backbone.View.define("NamedView", {
+			initialize: function(models, options) {
 
-		}
-	}, {
-		classProperty: true
-	});
-	ok(typeof(Backbone.Views.NamedView) !== undefined, ".Views.NamedView exists");
+			}
+		}, {
+			classProperty: true
+		});
 
-	Backbone.Router.define("NamedRouter", {
-		initialize: function(models, options) {
+		assert.notEqual(Backbone.Views.NamedView, undefined)
+	},
 
-		}
-	}, {
-		classProperty: true
-	});
-	ok(typeof(Backbone.Routers.NamedRouter) !== undefined, ".Routes.NamedRoute exists");
-});
+	".Routes.NamedRoute exists": function() {
+		Backbone.Router.define("NamedRouter", {
+			initialize: function(models, options) {
 
-test("Class Property Set", function() {
-	ok(Backbone.Collections.NamedCollection.classProperty, "Collections.NamedCollection.classProperty === true");
-	ok(Backbone.Models.NamedModel.classProperty, "Models.NamedModel.classProperty === true");
-	ok(Backbone.Views.NamedView.classProperty, "Views.NamedView.classProperty === true");
-	ok(Backbone.Routers.NamedRouter.classProperty, "Routers.NamedRouter.classProperty === true");
-});
+			}
+		}, {
+			classProperty: true
+		});
 
-test("Creating Named Subsclass", function() {
-	Backbone.Collections.NamedCollection.define("AnotherNamedCollection", {}, {classProperty: true});
-	Backbone.Models.NamedModel.define("AnotherNamedModel", {}, {classProperty: true});
-	Backbone.Views.NamedView.define("AnotherNamedView", {}, {classProperty: true});
-	Backbone.Routers.NamedRouter.define("AnotherNamedRouter", {}, {classProperty: true});
+		assert.notEqual(Backbone.Routers.NamedRouter, undefined)
+	}
+}).run();
 
-	ok(Backbone.Collections.AnotherNamedCollection, "AnotherNamedCollection exists");
-	ok(Backbone.Models.AnotherNamedModel, "AnotherNamedModel exists");
-	ok(Backbone.Views.AnotherNamedView, "AnotherNamedView exists");
-	ok(Backbone.Routers.AnotherNamedRouter, "AnotherNamedRouter exists");
+vows.describe("Class Property Set").addBatch({
+"Collections.NamedCollection.classProperty === true": function() { assert.equal(Backbone.Collections.NamedCollection.classProperty, true); },
+"Models.NamedModel.classProperty === true": function() { assert.equal(Backbone.Models.NamedModel.classProperty, true); },
+"Views.NamedView.classProperty === true": function() { assert.equal(Backbone.Views.NamedView.classProperty, true); },
+"Routers.NamedRouter.classProperty === true": function() { assert.equal(Backbone.Routers.NamedRouter.classProperty, true); },
+}).run();
+
+vows.describe("Creating Named Subsclass").addBatch({
+	"AnotherNamedCollection exists": function() {
+		Backbone.Collections.NamedCollection.define("AnotherNamedCollection", {}, {classProperty: true});
+		assert.equal(Backbone.Collections.AnotherNamedCollection, true); 
+	},
+
+	"AnotherNamedModel exists": function() {
+		Backbone.Models.NamedModel.define("AnotherNamedModel", {}, {classProperty: true});
+		assert.equal(Backbone.Models.AnotherNamedModel, true); 
+	},
+
+	"AnotherNamedView exists": function() {
+		Backbone.Views.NamedView.define("AnotherNamedView", {}, {classProperty: true});
+		assert.equal(Backbone.Views.AnotherNamedView, true); 
+	},
+
+	"AnotherNamedRouter exists": function() {
+		Backbone.Routers.NamedRouter.define("AnotherNamedRouter", {}, {classProperty: true});
+		assert.equal(Backbone.Routers.AnotherNamedRouter, true); 
+	}
 });
